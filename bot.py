c2af1ed3c961f3eb0d7f3db5557c72db8ab07dd2
nurmuhammad <nurmuhammadhasanov0@gmail.com>
2023-03-18 19:55:37 +0500

    finished

---
+++ bot.py
@@ -0,0 +1,36 @@
+"""
+This is a echo bot.
+It echoes any incoming text messages.
+"""
+import wikipedia
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '6203141105:AAG1SSFOe-ffNk_k1END3bVY7TJJDNYm4LA'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+wikipedia.set_lang("uz")
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hey wikipedia botiga xush kelibsiz")
+
+
+
+@dp.message_handler()
+async def send_wiki(message: types.Message):
+    try:
+        response = wikipedia.summary(message.text)
+    except:
+        response = "Bunday maqola topilmadi"
+    await message.answer(response)
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
